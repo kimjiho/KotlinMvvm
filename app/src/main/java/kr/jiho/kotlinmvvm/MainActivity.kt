78e20f40654d540b3a5f37f4618e4deab3a75e6b
KimJiho <kjh325kr@gmail.com>
2021-12-31 09:45:43 +0900

    update : first project commit

---
+++ app/src/main/java/kr/jiho/kotlinmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package kr.jiho.kotlinmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
