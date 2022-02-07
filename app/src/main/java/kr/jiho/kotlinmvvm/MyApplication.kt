b1d777cd03f71edf8204aa29da1b1f318d504cd2
김지호 <kjh325@hunet.co.kr>
2022-02-07 18:02:51 +0900

    update : Hilt 셋팅중

---
+++ app/src/main/java/kr/jiho/kotlinmvvm/MyApplication.kt
@@ -0,0 +1,8 @@
+package kr.jiho.kotlinmvvm
+
+import androidx.multidex.MultiDexApplication
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApplication : MultiDexApplication() {
+}
